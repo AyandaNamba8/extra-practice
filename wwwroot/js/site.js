fcc56bf642733562fe4e10b122ee123ea7c84010
AyandaNamba8 <89146595+AyandaNamba8@users.noreply.github.com>
2024-01-11 14:10:45 +0200

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const display = document.querySelector('.display');
+const clearDisplay = () => { display.value = ''; };
+const addNumber = number => { display.value += number; };
+const addOperator = operator => { display.value += operator; };
+const addDecimal = () => { display.value += '.'; };
+
+
+const result = () => {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+};
